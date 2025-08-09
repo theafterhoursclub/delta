f7e7946dde513dfd83bc0af8ca7de72feaabdf0e
ll_admin <phil.m.harrison@hotmail.co.uk>
2025-08-09 17:05:09 +0100

    feat(admin): register Task model with admin interface
    * Add TaskAdmin class to manage Task model in admin
    * Configure list display, filters, search fields, and ordering

--- kanban/admin.py
+++ kanban/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "due_date", "created_at", "updated_at", "order")
+    list_filter = ("status", "due_date")
+    search_fields = ("title", "description")
+    ordering = ("status", "order", "due_date")
