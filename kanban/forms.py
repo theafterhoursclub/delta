6d8e3270c65321f5105fda91b8cab36909b648d6
ll_admin <phil.m.harrison@hotmail.co.uk>
2025-08-09 18:05:26 +0100

    docs(forms): add docstrings for TaskForm and its fields
    * Added a module-level docstring for forms.py.
    * Added a class docstring for TaskForm.
    * Documented the purpose of each field in the form.

--- kanban/forms.py
+++ kanban/forms.py
@@ -1,19 +1,22 @@
+"""Forms for the kanban app."""
+
 from django import forms
 from .models import Task
 
 
 class TaskForm(forms.ModelForm):
-    class Meta:
+    """Form for creating and updating Task instances."""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Class fields/Meta"""
+
         model = Task
         fields = ["title", "description", "status", "due_date"]
         widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control"}),
+            "status": forms.Select(attrs={"class": "form-control"}),
             "due_date": forms.DateInput(
                 attrs={"type": "date", "class": "form-control"}
             ),
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for name, field in self.fields.items():
-            if name != "due_date":
-                field.widget.attrs["class"] = "form-control"
