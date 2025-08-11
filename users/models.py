98dbf4e1465e729e3e0d3bbd05a78459ef32c403
ll_admin <phil.m.harrison@hotmail.co.uk>
2025-08-11 17:34:23 +0100

    feat(models): add Organisation and Team models
    * Introduced Organisation model to represent teams and users' affiliations.
    * Added Team model to establish a relationship with Organisation.
    * Updated CustomUser model to include a foreign key to Team.

--- users/models.py
+++ users/models.py
@@ -1,9 +1,31 @@
+"""
+User Models contains everything required for a user/org/team
+"""
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Organisation(models.Model):
+    """Every team and user must be part of an organisations"""
+
+    name = models.CharField(max_length=100)
+
+
+class Team(models.Model):
+    """Every team member must be part of a team, and a team must be part of an organisation"""
+
+    name = models.CharField(max_length=100)
+    team = models.ForeignKey("Organisation", on_delete=models.CASCADE)
+
+
 class CustomUser(AbstractUser):
-    # Fields inherited from AbstractUser:
-    # username, first_name, last_name, email
-    # is_staff or is_superuser
+    """Custom User inherits from AbstractUser. Fields include:
+    - username, first_name, last_name, email
+    - is_staff or is_superuser
+    """
+
     bio = models.TextField(blank=True, null=True)
+    team = models.ForeignKey(
+        Team, on_delete=models.SET_NULL, null=True, blank=True, related_name="users"
+    )
